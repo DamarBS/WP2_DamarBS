e15e3b93faa61d5c090c4635691d7d9d80de0944
DamarBS <damarbagus2001@gmail.com>
2022-04-21 13:00:00 +0700

    tugas 5

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Damar</li>
+        <li>Nama Belakang : Damar</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Village</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tangerang</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Lari</li>
+    </ul>
+</section>
